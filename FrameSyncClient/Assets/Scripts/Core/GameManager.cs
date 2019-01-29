be96a6fbd61f665ac15869e7ce7a3328a06ec824
chanyowthea <454982165@qq.com>
2019-01-29 20:55:19 +0800

    change directory name

---
+++ FrameSyncClient/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Msg;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    Dictionary<int, UDPFrameData> _FrameMessages = new Dictionary<int, UDPFrameData>();
+    Dictionary<int, UDPFrameData> _PartialFrameMessages = new Dictionary<int, UDPFrameData>();
+    readonly float _UpdateDeltaTime = 0.02f;
+    float _LapsedTime;
+    public int CurGameFrame { private set; get; }
+    readonly int _MaxSmoothFrame = 3;
+    int _CurSmoothFrame = 0;
+    readonly int _MaxForwardTimePerFrame = 5;
+
+    void Awake()
+    {
+        GameSingleton.Init();
+        GameSingleton._GameService.AddCallback<UDPMoveStart>(OnProcessMsg);
+        GameSingleton._GameService.AddCallback<UDPMoveEnd>(OnProcessMsg);
+        GameSingleton._GameService.AddCallback<UDPChangeDir>(OnProcessMsg);
+        GameSingleton._GameService.AddCallback<UDPReleaseSkill>(OnProcessMsg);
+        GameSingleton._GameService.AddCallback<UDPFrameData>(OnProcessMsg);
+    }
+
+    void Update()
+    {
+        if (_LapsedTime < _UpdateDeltaTime)
+        {
+            _LapsedTime += Time.deltaTime;
+            return;
+        }
+        _LapsedTime = 0; 
+        FrameForward();
+    }
+
+    void OnDestroy()
+    {
+        GameSingleton._GameService.RemoveCallback<UDPFrameData>(OnProcessMsg);
+        GameSingleton._GameService.RemoveCallback<UDPMoveStart>(OnProcessMsg);
+        GameSingleton._GameService.RemoveCallback<UDPMoveEnd>(OnProcessMsg);
+        GameSingleton._GameService.RemoveCallback<UDPChangeDir>(OnProcessMsg);
+        GameSingleton._GameService.RemoveCallback<UDPReleaseSkill>(OnProcessMsg);
+        GameSingleton.Clear();
+    }
+
+    void FrameForward()
+    {
+        int lastestFrameNumber = 0;
+        if (_FrameMessages.Count > 0)
+        {
+            lastestFrameNumber = _FrameMessages.ElementAt(_FrameMessages.Count - 1).Value.FrameNumber;
+        }
+        int gap = lastestFrameNumber - CurGameFrame;
+        if (gap < 0)
+        {
+            gap = 0;
+        }
+        for (int k = 0; k < Mathf.Clamp(gap, 1, _MaxForwardTimePerFrame); k++)
+        {
+            if (_CurSmoothFrame == 0)
+            {
+                if (_FrameMessages.Count == 0)
+                {
+                    return;
+                }
+
+                var message = _FrameMessages.ElementAt(0).Value;
+                _FrameMessages.Remove(message.FrameNumber);
+                Debug.Log("FrameNumber=" + message.FrameNumber);
+                for (int i = 0, length = message.Msgs.Count; i < length; i++)
+                {
+                    GameSingleton._GameService.ProcessReceivedMessage(message.Msgs[i].ToByteArray());
+                }
+                PlayerController.Instance.FrameForward();
+                GameSingleton._PhysicSystem.UpdateCollider();
+                CurGameFrame += 1;
+                _CurSmoothFrame += 1;
+            }
+            else
+            {
+                PlayerController.Instance.FrameForward();
+                GameSingleton._PhysicSystem.UpdateCollider();
+                _CurSmoothFrame += 1;
+                if (_CurSmoothFrame == _MaxSmoothFrame)
+                {
+                    _CurSmoothFrame = 0;
+                }
+            }
+        }
+    }
+
+    void OnProcessMsg(UDPFrameData message)
+    {
+        if (_FrameMessages.Count > 0)
+        {
+            int lastFrame = _FrameMessages.ElementAt(_FrameMessages.Count - 1).Value.FrameNumber;
+            if (message.FrameNumber > lastFrame + 1)
+            {
+                // supply frame
+                _PartialFrameMessages.Add(message.FrameNumber, message);
+                // request frame [lastFrame+1, message.FrameNumber-1]
+            }
+            else if (message.FrameNumber == lastFrame + 1)
+            {
+                _FrameMessages.Add(message.FrameNumber, message);
+            }
+            // message.FrameNumber <= lastFrame
+            // use the newest message. 
+            else
+            {
+                int firstFrame = _FrameMessages.ElementAt(0).Value.FrameNumber;
+                if (message.FrameNumber >= firstFrame)
+                {
+                    _FrameMessages[message.FrameNumber] = message;
+                }
+            }
+        }
+    }
+
+    void OnProcessMsg(UDPMoveStart msg)
+    {
+        PlayerController.Instance._Player.StartMove();
+    }
+
+    void OnProcessMsg(UDPMoveEnd msg)
+    {
+        PlayerController.Instance._Player.EndMove();
+    }
+
+    void OnProcessMsg(UDPChangeDir msg)
+    {
+        PlayerController.Instance._Player.ChangeDir(msg.Angle);
+    }
+
+    void OnProcessMsg(UDPReleaseSkill msg)
+    {
+        Debug.Log("OnProcessMsg UDPReleaseSkill.SkillId=" + msg.SkillId);
+    }
+}
---
+++ FrameSyncServer/Facade.cs
@@ -0,0 +1,244 @@
+﻿using Msg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFrameSync
+{
+    public struct GatePlayerInfo
+    {
+        static int _CurInternalIndex;
+        public UserToken _UserToken;
+        public int UserId { private set; get; }
+
+        public GatePlayerInfo(UserToken token)
+        {
+            _UserToken = token;
+            _CurInternalIndex++;
+            UserId = _CurInternalIndex;
+        }
+    }
+
+    public struct GamePlayerInfo
+    {
+        public int _UserId;
+        public int _RoomId;
+    }
+
+    class Facade
+    {
+        public static Facade Instance { private set; get; }
+
+        public GateServer _GateServer { private set; get; }
+        public GameServer _GameServer { private set; get; }
+        List<GameRoom> _GameRooms = new List<GameRoom>();
+        Dictionary<int, GatePlayerInfo> _GatePlayerInfos = new Dictionary<int, GatePlayerInfo>();
+
+        public void Init()
+        {
+            Instance = this;
+            StartGateServer();
+            StartGameServer();
+            _GateServer._OnConnect += OnConnect;
+            _GateServer._OnDisconnect += OnDisconnect;
+            _GameServer._OnReceiveMsg += OnReceivedGameMsg;
+        }
+
+        public void Clear()
+        {
+            _GateServer._OnConnect -= OnConnect;
+            _GateServer._OnDisconnect -= OnDisconnect;
+            _GameServer._OnReceiveMsg -= OnReceivedGameMsg;
+            StopGameServer();
+            StopGateServer();
+        }
+
+        void OnConnect(UserToken token)
+        {
+            var info = new GatePlayerInfo(token);
+            _GatePlayerInfos.Add(info.UserId, info);
+        }
+
+        void OnDisconnect(TcpClient client)
+        {
+            var e = _GatePlayerInfos.GetEnumerator();
+            int userId = 0;
+            while (e.MoveNext())
+            {
+                if (e.Current.Value._UserToken._TcpClient == client)
+                {
+                    client.Close(); 
+                    _GatePlayerInfos.Remove(e.Current.Value.UserId);
+                    break; 
+                }
+            }
+        }
+
+        void OnReceivedGameMsg(byte[] msg, IPEndPoint ip)
+        {
+            // find client
+            var e = _GatePlayerInfos.GetEnumerator();
+            int userId = 0;
+            while (e.MoveNext())
+            {
+                if (e.Current.Value._UserToken._GameIp == ip)
+                {
+                    userId = e.Current.Value.UserId;
+                }
+            }
+            if (userId != 0)
+            {
+                for (int i = 0, length = _GameRooms.Count; i < length; i++)
+                {
+                    var room = _GameRooms[i];
+                    for (int j = 0, max = room._UserIds.Length; j < max; j++)
+                    {
+                        if (room._UserIds[j] == userId)
+                        {
+                            room.Receive(msg);
+                            return; 
+                        }
+                    }
+                }
+            }
+        }
+
+        void StartGateServer()
+        {
+            _GateServer = new GateServer();
+            _GateServer.Start();
+            _GateServer.AddCallback<LoginReq>(LoginReqCallback);
+        }
+
+        void StopGateServer()
+        {
+            _GateServer.RemoveCallback<LoginReq>(LoginReqCallback);
+            _GateServer.Stop();
+        }
+
+        void LoginReqCallback(LoginReq message, TcpClient client)
+        {
+            Console.WriteLine("login req name=" + message.AccountName);
+            _GateServer.Send(new LoginRes { Rs = true }, client);
+
+            // TODO 目前只能是两个人进行测试
+            if (_GatePlayerInfos.Count >= 2)
+            {
+                Console.WriteLine("match successfully! ");
+
+                List<PlayerInfo> list = new List<PlayerInfo>();
+                List<int> userIds = new List<int>();
+                var ep = _GatePlayerInfos.GetEnumerator();
+                while (ep.MoveNext())
+                {
+                    var info = new PlayerInfo();
+                    info.PlayerId = ep.Current.Value.UserId;
+                    info.PlayerName = "PlayerName" + info.PlayerId;
+                    info.RoleId = 1;
+                    list.Add(info);
+
+                    userIds.Add(ep.Current.Value.UserId);
+                }
+
+                // create room
+                var room = new GameRoom();
+                room.Init(userIds.ToArray());
+
+                // send match info
+                var matchRes = new MatchRes();
+                matchRes.PlayerInfos.AddRange(list);
+                var e = _GatePlayerInfos.GetEnumerator();
+                while (e.MoveNext())
+                {
+                    _GateServer.Send(matchRes, e.Current.Value._UserToken._TcpClient);
+                }
+            }
+        }
+
+        void StartGameServer()
+        {
+            _GameServer = new GameServer();
+            _GameServer.Start();
+        }
+
+        void StopGameServer()
+        {
+            _GameServer.Stop();
+        }
+
+        public UserToken GetClient(TcpClient client)
+        {
+            var e = _GatePlayerInfos.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (e.Current.Value._UserToken._TcpClient == client)
+                {
+                    return e.Current.Value._UserToken;
+                }
+            }
+            return default(UserToken);
+        }
+
+        public UserToken GetClient(IPEndPoint gateIp)
+        {
+            var e = _GatePlayerInfos.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (e.Current.Value._UserToken.GateIP == gateIp)
+                {
+                    return e.Current.Value._UserToken;
+                }
+            }
+            return default(UserToken);
+        }
+
+        public UserToken GetClient(int userId)
+        {
+            var e = _GatePlayerInfos.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (e.Current.Value.UserId == userId)
+                {
+                    return e.Current.Value._UserToken;
+                }
+            }
+            return default(UserToken);
+        }
+
+        void AddClient(UserToken token)
+        {
+            var e = _GatePlayerInfos.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (e.Current.Value._UserToken._TcpClient == token._TcpClient)
+                {
+                    Console.WriteLine("e.Current.Value._UserToken._TcpClient == token._TcpClient");
+                    return;
+                }
+            }
+
+            GatePlayerInfo info = new GatePlayerInfo(token);
+            _GatePlayerInfos.Add(info.UserId, info);
+        }
+
+        void RemoveClient(TcpClient client)
+        {
+            var e = _GatePlayerInfos.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (e.Current.Value._UserToken._TcpClient == client)
+                {
+                    client.Close();
+                    _GatePlayerInfos.Remove(e.Current.Key);
+                    Console.WriteLine("[INFO]client disconnect! ip address=" + client.Client.RemoteEndPoint);
+                    break;
+                }
+            }
+        }
+
+    }
+}
